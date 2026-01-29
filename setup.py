3bde7c8c2ba1571ce71c056ebaa228f9b924b23b
haptein <bruno.alatriste@gmail.com>
2026-01-29 01:07:40 -0600

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="communityone",
-    version="1.1.1",
+    version="1.2.0",
     author="CommunityOne",
     author_email="support@communityone.io",
     description="Official Python SDK for CommunityOne API",
